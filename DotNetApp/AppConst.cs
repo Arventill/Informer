eda6ee0b7dc50e9917946ea688356acffba42e5a
Filip Nowicki <filip.nowicki1999@gmail.com>
2019-01-04 23:01:00 +0100

    Stworzenie Projektu

---
+++ DotNetApp/AppConst.cs
@@ -0,0 +1,16 @@
+﻿using DotNetApp.Resources;
+using System;
+using System.Collections.Generic;
+
+namespace DotNetApp
+{
+    class AppConst
+    {
+        private const string Tab = "\t";
+
+        public static readonly Dictionary<string, ConsoleColor> AppAboutInfo = new Dictionary<string, ConsoleColor>
+        {
+            {$"{Tab}Wersja aplikacji: {AppResources.ver}",ConsoleColor.Yellow }
+        };
+    }
+}
---
+++ DotNetApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using DotNetApp.Resources;
+using System;
+
+namespace DotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            About();
+
+        }
+        private static void About()
+        {
+            foreach (var item in AppConst.AppAboutInfo)
+            {
+                Console.ForegroundColor = item.Value;
+                Console.WriteLine(item.Key);
+            }
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
