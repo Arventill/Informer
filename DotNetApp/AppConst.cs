ed1fa7db0d179dc13f874f552c06382f1aaa66aa
Filip Nowicki <filip.nowicki1999@gmail.com>
2019-01-08 14:19:03 +0100

    Stworzenie szkieletu aplikacji

--- DotNetApp/AppConst.cs
+++ DotNetApp/AppConst.cs
@@ -8,6 +8,16 @@ namespace DotNetApp
     {
         private const string Tab = "\t";
 
+        public static void Welcome()
+        {
+            Console.WriteLine("Witaj w aplikacji!\n");
+            Console.WriteLine("Wybierz jedna z dostepnych opcji:");
+            Console.WriteLine("[1] Opcja nr 1");
+            Console.WriteLine("[2] Informacje o aplikacji");
+            Console.WriteLine("[3] Wyjscie\n");
+            Console.Write("Twoj wybor: ");
+        }
+
         public static readonly Dictionary<string, ConsoleColor> AppAboutInfo = new Dictionary<string, ConsoleColor>
         {
             {$"{Tab}Wersja aplikacji: {AppResources.ver}",ConsoleColor.Yellow }
--- DotNetApp/Program.cs
+++ DotNetApp/Program.cs
@@ -7,9 +7,42 @@ namespace DotNetApp
     {
         static void Main(string[] args)
         {
-            About();
+            while (true)
+            {
+                AppConst.Welcome();
+                var answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("In progress");
+                            break;
+                        }
+                    case "2":
+                        {
+                            About();
+                            break;
+                        }
+                    case "3":
+                        {
+                            ExitApp();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Nie znaleziono takiej opcji!");
+                            break;
+                        }
+                }
+            }
 
         }
+
+        private static void ExitApp()
+        {
+            System.Environment.Exit(0);
+        }
+
         private static void About()
         {
             foreach (var item in AppConst.AppAboutInfo)
